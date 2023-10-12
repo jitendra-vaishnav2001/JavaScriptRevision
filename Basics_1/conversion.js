ae6290abbc00a0b9418c36eae4222abc48f008d3
Jitendra Vaishnav <135482799+jitendra-vaishnav2001@users.noreply.github.com>
2023-10-12 04:38:01 +0000

    conversion one data type to another

---
+++ Basics_1/conversion.js
@@ -0,0 +1,19 @@
+
+
+//conversion one type to another
+
+    // let a="3";
+    // console.log(typeof (a));
+    // let aa=Number(a);
+    // console.log(typeof (aa));
+
+    // let b=33;
+    // console.log(typeof(b));
+    // let b2=String(b);
+    // console.log("converted "+ typeof(b2));
+
+    let c=2;
+    let c2=Boolean(c);
+    let d=0;
+    let d2=Boolean(d);
+    console.log(" after converting both  c2 and d2 is " + c2 + " , " +d2);
