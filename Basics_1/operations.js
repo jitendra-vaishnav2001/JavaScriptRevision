e3b2d53b8a8a6f8359a1a439fe97adee4ea3fe5a
Jitendra Vaishnav <135482799+jitendra-vaishnav2001@users.noreply.github.com>
2023-10-12 04:42:43 +0000

    some operation

---
+++ Basics_1/operations.js
@@ -0,0 +1,22 @@
+//some operations 
+
+    console.log(3+3+"3"+3);
+    //In this case before the string value is added and after string value is not added include string value 
+    console.log(3-"3"+13-"10");
+
+    
+    // string + number = concatenated string
+    // number + number = the sum of both numbers
+    // string - number = the difference between (coerced string) and the number
+
+    // explanation:
+    // If one or both operands is string, then the plus is considered as string concatenation operator rather than adding numbers.
+    // the minus operator always try to convert both operands to numbers.
+
+    // console.log(null >=0);
+    // console.log(null>0);
+    // console.log(null==0);
+    // console.log(null<0);
+    // console.log(null<=0);
+
+    //this is beacuse equillity check == and comparison ><>= are work differntly they can't give us prditble result
