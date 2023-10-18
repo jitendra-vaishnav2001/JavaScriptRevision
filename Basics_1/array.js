85bd34a57ee7651daa04b52926054b294150623a
Jitendra Vaishnav <135482799+jitendra-vaishnav2001@users.noreply.github.com>
2023-10-18 17:43:33 +0000

    about array

---
+++ Basics_1/array.js
@@ -0,0 +1,25 @@
+// const array=[1,2,3,"jitu0",false];
+
+// const arr1=[1,2,3,4,5]
+// console.log(arr1[0]);
+// arr1.push(2);
+// arr1.push(3);
+// console.log(arr1);
+// arr1.pop();
+// console.log(arr1);
+
+// const array=[1,2,3,4,5];
+// const array2=[7,8,9,10,11]
+
+// const array3=array.concat(array2);
+// console.log(array.includes(8))
+// const arr3=array.slice(0,3)
+// const arr4=array.splice(0,3)
+
+// console.log(arr3)
+// console.log(arr4)
+// console.log(array)
+
+const array
+
+
